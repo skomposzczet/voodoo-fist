2036f7e775dcf75c8251829cedcbba5f6a23c2d7
skomposzczet <72391558+skomposzczet@users.noreply.github.com>
2023-03-05 21:11:31 +0100

    add from document contructor for user

--- application/server/src/model/user.rs
+++ application/server/src/model/user.rs
@@ -1,5 +1,6 @@
 use serde::{Serialize, Deserialize};
-use mongodb::bson::oid::ObjectId;
+use mongodb::bson::{Document, Bson, oid::ObjectId};
+use std::error::Error;
 
 #[derive(Serialize, Deserialize, Debug)]
 pub struct User {
@@ -13,5 +14,9 @@ pub struct User {
 impl User {
     pub fn new(email: &String, username: &String, password: &String) -> User {
         User {id: None, email: email.clone(), username: username.clone(), password: password.clone() }
+    }
+
+    pub fn from_document(document: Document) -> Option<User> {
+        bson::from_bson(Bson::Document(document)).ok()
     }
 }

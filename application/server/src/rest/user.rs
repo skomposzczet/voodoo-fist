fcbd2ff5610eb731363b6ae8d1e3d7696883f211
skomposzczet <72391558+skomposzczet@users.noreply.github.com>
2023-03-07 21:25:11 +0100

    add login endpoint and handler

--- application/server/src/rest/user.rs
+++ application/server/src/rest/user.rs
@@ -1,13 +1,15 @@
-use warp::{Filter, reject::Reject, Rejection};
+use warp::{Filter, reject::Reject, Rejection, reply::Json};
 use std::sync::Arc;
 use crate::model::{Db, user::User};
-use serde::Deserialize;
-use crate::security::hash::hashed_password;
+use serde::{Deserialize, Serialize};
+use crate::security::{hash::hashed_password, token};
 
 #[derive(Debug)]
 enum Error{
     NotUniqueError,
-    InnerError
+    InnerError,
+    Unauthorized,
+    NoUserWithSuchEmail
 }
 impl Reject for Error {}
 
@@ -18,18 +20,46 @@ struct RegisterBody {
     password: String
 }
 
+#[derive(Deserialize, Debug)]
+struct LoginBody {
+    email: String,
+    password: String
+}
+
+#[derive(Serialize)]
+pub struct LoginResponse {
+    pub jwtoken: String,
+}
+
 pub fn account_paths(db: Arc<Db>) -> impl Filter<Extract = impl warp::Reply, Error = Rejection> + Clone {
     let with_db = warp::any().map(move || db.clone());
 
     let register = warp::path("register")
         .and(warp::post())
         .and(warp::path::end())
-        // .and(warp::any().map(move || db.clone()))
         .and(with_db.clone())
         .and(warp::body::json())
         .and_then(register_handle);
 
-    register
+    let login = warp::path("login")
+        .and(warp::post())
+        .and(warp::path::end())
+        .and(with_db.clone())
+        .and(warp::body::json())
+        .and_then(login_handle);
+
+    register.or(login)
+}
+
+async fn login_handle(db: Arc<Db>, body: LoginBody) -> Result<Json, Rejection> {
+    let user = User::get_by_email(&db, &body.email).await
+        .map_err(|_| warp::reject::custom(Error::NoUserWithSuchEmail))?;
+    if !user.password_matches(&hashed_password(&body.password)) {
+        return Err(warp::reject::custom(Error::Unauthorized));
+    }
+    let token = token::create_jwt(&user)
+        .map_err(|_| Error::InnerError)?;
+    Ok(warp::reply::json( &LoginResponse {jwtoken: token}))
 }
 
 async fn register_handle(db: Arc<Db>, body: RegisterBody) -> Result<String, Rejection> {

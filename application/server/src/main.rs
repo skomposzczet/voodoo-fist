201e0e488e1d44abcea7085f5dbd26a6766ab868
skomposzczet <72391558+skomposzczet@users.noreply.github.com>
2023-03-08 21:18:31 +0100

    setup basic server

--- application/server/src/main.rs
+++ application/server/src/main.rs
@@ -1,3 +1,20 @@
-fn main() {
-    println!("Hello, world!");
+#![allow(unused)]
+
+mod model;
+mod security;
+mod rest;
+
+use std::error::Error;
+use model::Db; 
+use std::sync::Arc;
+
+#[tokio::main]
+async fn main() -> Result<(), Box<dyn Error>> {
+    println!("Connecting do db...");
+    let db = Arc::new(model::db::init_db().await);
+    println!("Successfully connected to db.");
+
+    warp::serve(rest::user::account_paths(db))
+        .run(([127, 0, 0, 1], 8000)).await;
+    Ok(())
 }

afd49122d384db6a00683d1f9382c04968253d68
skomposzczet <72391558+skomposzczet@users.noreply.github.com>
2023-03-08 20:51:16 +0100

    add dashboard route and handle

--- application/server/src/rest/user.rs
+++ application/server/src/rest/user.rs
@@ -1,15 +1,18 @@
 use warp::{Filter, reject::Reject, Rejection, reply::Json};
 use std::sync::Arc;
-use crate::model::{Db, user::User};
+use crate::{model::{Db, user::User}, security::token::{jwt_from_header, decode_jwt}};
 use serde::{Deserialize, Serialize};
+use serde_json::json;
 use crate::security::{hash::hashed_password, token};
+use std::str::FromStr;
 
 #[derive(Debug)]
 enum Error{
     NotUniqueError,
     InnerError,
     Unauthorized,
-    NoUserWithSuchEmail
+    NoUserWithSuchEmail,
+    InvalidHeader
 }
 impl Reject for Error {}
 
@@ -48,7 +51,14 @@ pub fn account_paths(db: Arc<Db>) -> impl Filter<Extract = (impl warp::Reply,), 
         .and(warp::body::json())
         .and_then(login_handle);
 
-    register.or(login)
+    let dashboard = warp::path("dashboard")
+        .and(warp::get())
+        .and(warp::path::end())
+        .and(with_db.clone())
+        .and(warp::header::<String>("Authorization"))
+        .and_then(dashboard_handle);
+
+    register.or(login).or(dashboard)
 }
 
 async fn login_handle(db: Arc<Db>, body: LoginBody) -> Result<Json, Rejection> {
@@ -74,4 +84,18 @@ async fn register_handle(db: Arc<Db>, body: RegisterBody) -> Result<String, Reje
 
 async fn is_unique_email(db: &Db, email: &String) -> bool {
     true
+}
+
+async fn dashboard_handle(db: Arc<Db>, auth_header: String) -> Result<Json, Rejection> {
+    let token = jwt_from_header(&auth_header)
+        .ok_or(Error::InvalidHeader)?;
+
+    let token_data = decode_jwt(&token)
+        .map_err(|_| Error::InnerError)?;
+
+    let user = User::get_by_id(&db, &token_data.claims.sub()).await
+        .map_err(|_| Error::InnerError)?;
+
+    let response = json!({"username": user.username()});
+    Ok(warp::reply::json(&response))
 }

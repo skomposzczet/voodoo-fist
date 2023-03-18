7f221ac5286d14c22fa60cfbf854611001a00862
skomposzczet <72391558+skomposzczet@users.noreply.github.com>
2023-03-18 12:42:40 +0100

    add cors and logging

--- application/server/src/main.rs
+++ application/server/src/main.rs
@@ -1,3 +1,5 @@
+extern crate pretty_env_logger;
+
 mod model;
 mod security;
 mod rest;
@@ -5,14 +7,25 @@ mod error;
 
 use std::error::Error;
 use std::sync::Arc;
+use warp::Filter;
 
 #[tokio::main]
 async fn main() -> Result<(), Box<dyn Error>> {
+    pretty_env_logger::init();
     println!("Connecting do db...");
     let db = Arc::new(model::db::init_db().await);
     println!("Successfully connected to db.");
 
-    warp::serve(rest::routes(db.clone()))
-        .run(([127, 0, 0, 1], 8000)).await;
+    let cors = warp::cors()
+        .allow_any_origin();
+
+    let log = warp::log("server::vf");
+
+    let routes = rest::routes(db.clone())
+        .with(cors)
+        .with(log);
+
+    warp::serve(routes)
+        .run(([0, 0, 0, 0], 8000)).await;
     Ok(())
 }

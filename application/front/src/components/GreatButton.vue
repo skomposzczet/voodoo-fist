de76091fae8c5747414f7f56a079d514aa1ea124
skomposzczet <72391558+skomposzczet@users.noreply.github.com>
2023-03-22 22:28:37 +0100

    add button component

---
+++ application/front/src/components/GreatButton.vue
@@ -0,0 +1,27 @@
+<template>
+    <button @click="onClick()" class="btn"> {{ text }} </button>
+</template>
+
+<script lang="ts">
+export default {
+    name: 'GreatButton',
+    props: {
+        text: String,
+    },
+    methods: {
+        onClick() {
+            this.$emit('btn-click')
+        },
+    },
+}
+</script>
+
+<style scoped>
+.btn{
+    box-shadow:inset 0 0 0 99999px rgba(0,0,0,0.2);
+    font-size: 18px;
+}
+.btn:hover{
+    box-shadow:none;
+}
+</style>

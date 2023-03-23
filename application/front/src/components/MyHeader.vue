d364125fa7306522b8c019298daf67fcb2ae323a
skomposzczet <72391558+skomposzczet@users.noreply.github.com>
2023-03-23 20:03:15 +0100

    add logout button

--- application/front/src/components/MyHeader.vue
+++ application/front/src/components/MyHeader.vue
@@ -3,6 +3,7 @@
         <router-link to="/" >
             <h1>{{ title }}</h1>
         </router-link>
+        <a v-if="$store.state.auth.status" class="hvr" @click="logout">Logout</a>
     </header>
 </template>
 
@@ -14,15 +15,20 @@ export default defineComponent({
     props: {
         title: String,
     },
+    methods: {
+        logout() {
+            this.$store.dispatch('auth/logout');
+            this.$router.push('/auth');
+        }
+    }
 });
 </script>
 
 <style scoped>
-    header {
-        display: flex;
-        justify-content: space-between;
-        align-items: center;
-        margin-bottom: 20px;
-    }
-
+header {
+    display: flex;
+    justify-content: space-between;
+    align-items: center;
+    margin-bottom: 20px;
+}
 </style>

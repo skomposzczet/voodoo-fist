80675fed88f45a50741fc478397dc05409f8a8cb
skomposzczet <72391558+skomposzczet@users.noreply.github.com>
2023-03-24 17:23:21 +0100

    add list enty component

---
+++ application/front/src/components/TodoList.vue
@@ -0,0 +1,74 @@
+<template >
+    <div class="listitem" @mouseover="toggle_hovered(true)" @mouseout="toggle_hovered(false)">
+        <div class="title">
+            <ColorPicker shape="circle" :pureColor="list.color" :disableAlpha="true" @update:pureColor="set_color"/>
+            <h2>{{ list.title }}</h2>
+        </div>
+        <Icon v-show="hovered" icon="ion:trash-sharp" class="delete" @click="delete_list" />
+    </div>
+</template>
+
+<script lang="ts">
+import { defineComponent, PropType } from "vue";
+import {Icon} from "@iconify/vue"
+import swal from 'sweetalert';
+import {List} from "@/api-types"
+
+export default defineComponent({
+    name: "ListItem",
+    components: {
+        Icon,
+    },
+    props: {
+        list: {
+            type: Object as PropType<List>,
+            required: true
+        },
+    },
+    data() {
+        return {
+            hovered: false,
+        }
+    },
+    methods: {
+        set_color(color: string) {
+            const vs = color.slice(4, -1).split(',').map(v => parseInt(v));
+            console.log('will change color ; ' + vs)
+        },
+        async delete_list() {
+            const accepted = await swal('You sure?', { buttons: ['nah', 'sure']});
+            if (accepted)
+                console.log('will delete list');
+        },
+        toggle_hovered(value: boolean) {
+            this.hovered = value;
+        },
+    },
+});
+</script>
+
+<style scoped>
+.listitem {
+    border-radius: 6px;
+    background-color: rgba(245, 245, 245, 0);
+    color: whitesmoke;
+    padding-left: 10px;
+    display: flex;
+    align-items: center;
+    justify-content: space-between;
+}
+.listitem:hover {
+    background-color: rgba(245, 245, 245, 0.1);
+}
+.title {
+    display: flex;
+    align-items: center;
+    justify-content: left;
+}
+.delete {
+    font-size: 20px;
+}
+.delete:hover { 
+    color: red;
+}
+</style>
--- application/front/src/main.ts
+++ application/front/src/main.ts
@@ -4,9 +4,12 @@ import router from './router'
 import store from './store'
 import axios from 'axios'
 import VueAxios from 'vue-axios'
+import Vue3ColorPicker from "vue3-colorpicker";
+import "vue3-colorpicker/style.css";
 
 createApp(App)
     .use(router)
     .use(store)
     .use(VueAxios, axios)
+    .use(Vue3ColorPicker)
     .mount('#app')

acb570bf467f71c012dc3555617033797a8c0472
skomposzczet <72391558+skomposzczet@users.noreply.github.com>
2023-03-24 20:00:30 +0100

    add disablable button component

---
+++ application/front/src/components/DisablableButton.vue
@@ -0,0 +1,32 @@
+<template>
+    <div>
+        <button :class="['btn', (is_disabled ? 'dis' : 'enb')]" :disabled="is_disabled" :title="reason">
+            {{ text }}
+        </button>
+    </div>
+</template>
+<script lang="ts">
+import { defineComponent } from 'vue';
+export default defineComponent({
+    name: "DisablableButton",
+    props: {
+        text: String,
+        is_disabled: Boolean,
+        reason: String,
+    },
+});
+</script>
+<style>
+.dis {
+    background-color: grey;
+}
+.dis:active {
+    transform: none;
+}
+.enb {
+    background-color: green;
+}
+.enb:hover {
+    background-color: rgb(0, 100, 0);
+}
+</style>

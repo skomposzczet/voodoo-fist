d9d72288e43d9ce26fc21a898a22e6c069423c8a
skomposzczet <72391558+skomposzczet@users.noreply.github.com>
2023-03-24 17:29:06 +0100

    add displaying username and lists on dashboard view

--- application/front/src/views/DashboardPage.vue
+++ application/front/src/views/DashboardPage.vue
@@ -1,20 +1,49 @@
 <template>
-    <h1>Home</h1>
+    <h2 v-if="username !== ''">{{ username }}'s dashboard</h2>
+    <UsersLists :lists="lists"/>
 </template>
 
 <script lang="ts">
 import { defineComponent } from 'vue';
 import authHeader from "../services/auth-header";
+import DataService from "../services/data-service"
+import {List} from "@/api-types"
+import UsersLists from "../components/UserLists.vue"
 
 export default defineComponent({
     name: 'DashboardPage',
-    async mounted() {
-        try {
-            const response = await this.axios.get('api/lists', { headers: authHeader() });
-            console.log(response);
-        } catch(err) {
-            this.$router.push('/auth')      
+    components: {
+        UsersLists,
+    },
+    data() {
+        return {
+            username: '',
+            lists: [] as List[],
         }
+    },
+    methods: {
+        async fetch_set_username() {
+            try {
+                const response = await this.axios.get('api/dashboard', { headers: authHeader() });
+                this.username = response.data.data.username;
+            } catch(err) {
+                this.$store.dispatch('auth/logout');
+                this.$router.push('/auth');
+            }
+        },
+        async fetch_set_lists() {
+            try {
+                const response = await DataService.get_lists();
+                this.lists = response.data.data.lists;
+                console.log(response);
+            } catch(err) {
+                console.log(err)
+            }
+        },
+    },
+    async created() {
+        await this.fetch_set_username();
+        await this.fetch_set_lists();
     },
 });
 </script>
